2ee2133010f098c56d43e996c93c34bf3c7dda58
Rick T <rick@pcrats.com>
2020-05-27 22:53:58 -0400

    moved decimal4 filter to tiles

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -93,6 +93,22 @@ export default {
       myBox() {
           this.attachPick(this)
       },
+  },
+  filters: {
+      decimal4(value, decimals) {
+        value = parseFloat(value)
+        if (!isFinite(value) || (!value && value !== 0)) return ''
+        decimals = decimals != null ? decimals : 4
+        var stringified = Math.abs(value).toFixed(decimals)
+        var _int = decimals
+            ? stringified.slice(0, -1 - decimals)
+            : stringified
+        var _float = decimals
+            ? stringified.slice(-1 - decimals)
+            : ''
+        var sign = value < 0 ? '-' : ''
+        return sign + _int + _float            
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueBabylonjs from 'vue-babylonjs'
-import {decimal4} from "@/decimal4"
+// import {decimal4} from "@/decimal4"
 
 Vue.config.productionTip = false
 Vue.use(VueBabylonjs)
-Vue.filter('decimal4', decimal4)
+// Vue.filter('decimal4', decimal4)
 
 new Vue({
   render: h => h(App),
